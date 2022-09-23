765e9ecd44ee1b19cec1da46d351249fc0ade23b
cecil <cecilghimire@gmail.com>
2022-09-23 16:16:03 +0545

    error handler

--- controller/user.js
+++ controller/user.js
@@ -1,11 +1,12 @@
 const User=require("../models/user")
+const errorHandler = require("./helper/dbErrorhandler")
 
 exports.signUp=async(req,res)=>{
     try{
     const user= await new User(req.body).save()
     res.send(user)
     }catch(err){
-    res.send(err)
+       res.json(errorHandler(err))
     }
    
     
