10a2644210be436d5c1a84c12d8f725f055c5dc7
cecil <cecilghimire@gmail.com>
2022-09-24 21:09:16 +0545

    bug in  /secret/::userId route

---
+++ routes/user.js
@@ -0,0 +1,17 @@
+const express=require('express');
+
+const { requireSignin } = require('../controller/auth');
+const { userById} = require('../controller/user');
+
+const router=express.Router()
+
+
+router.param('userId',userById)
+
+router.get('/secret/:userId',requireSignin,(req,res)=>{
+    console.log('ayo profilr bhiyr')
+    res.json({ user:req.profile })
+})
+
+
+module.exports=router;
