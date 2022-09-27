279c07f1b9b01405dd89cc413c51f8173ce23e75
cecil <cecilghimire@gmail.com>
2022-09-27 19:04:54 +0545

    added midleware to get product by id

--- controller/product.js
+++ controller/product.js
@@ -5,6 +5,22 @@ const formidable=require("formidable")
 const fs=require("fs");
 const product = require("../models/product");
 
+exports.productbyId=(req,res,next,id)=>{
+  Product.findById(id).exec((err,product)=>{
+    if(err|| !product){
+      return res.status(400),json({error:['not found product',err]})
+    }
+    req.product=product
+    next()
+  })
+ 
+
+}
+exports.read=(req,res)=>{
+   req.product.photo=undefined
+   return res.json(req.product )
+}
+
 exports.create=(req,res)=>{
     let form=new formidable.IncomingForm()
     form.keepExtensions=true
@@ -14,10 +30,18 @@ exports.create=(req,res)=>{
         return res.status(400).json({error:[err,'Image not uploaded']})
 
       }
-    //files.photo.path not working
+    //check feilds
+ //    const {name,description,price,category,quantity,shipping}=feild
+ 
+
     let newprod=new Product(feilds)
     console.log()
     if (files.photo){
+      if (files.photo.size>1000000){
+        return res.status(400).json({
+          error:"image should be less than 1MB"
+        })
+      }
       // console.log("fs.readFileSync(files.photo.filepath)")
         newprod.photo.data= fs.readFileSync(files.photo.filepath)
         newprod.photo.contentType=files.photo.mimetype
@@ -25,7 +49,8 @@ exports.create=(req,res)=>{
 
 
      newprod.save((err,data)=>{
-      if (err) return res.json({"error":err})
+      if (err) return res.json({
+        err})
       return res.json({data})
     })
 
--- routes/product.js
+++ routes/product.js
@@ -1,13 +1,16 @@
 const express=require('express');
 const { requireSignin, isAuth, isAdmin } = require('../controller/auth');
 
-const { create } = require('../controller/product');
+const { create, productbyId ,read} = require('../controller/product');
 const { userById } = require('../controller/user');
 
 
 const router=express.Router()
 
 router.post('/product/create/:userId',requireSignin ,isAuth,isAdmin,create);
-
+router.get('.product/:productId',read) 
 router.param('userId',userById)
+router.param('product',productbyId)
+
+
 module.exports=router;
