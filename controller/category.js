4c25a33b744812eed31d862d565dbd86d89d4769
cecil <cecilghimire@gmail.com>
2022-09-28 10:17:46 +0545

    added category upadet delete read readlist route and controller

--- controller/category.js
+++ controller/category.js
@@ -2,6 +2,23 @@ const errorHandler = require("../helper/dbErrorhandler");
 const Category = require("../models/catergories");
 
 
+
+exports.categoryById=(req,res,next,id)=>{
+
+    Category.findById(id).exec((err,cate)=>{ 
+     
+      if(err|| !cate){
+       
+        return res.status(400).json({error:['not found category'  ]})
+     }
+     
+      req.category=category
+      console.log('procuct id running')
+      next()
+    })
+  }
+
+
 exports.create=(req,res)=>{
      const category=new Category(req.body)
      category.save((err,data)=>{
@@ -13,3 +30,51 @@ exports.create=(req,res)=>{
          res.json({data});
      })
 };
+
+
+exports.read=(req,res)=>{
+      res.json({category:req.category})
+}
+
+
+exports.readList=(req,res)=>{
+    Category.find().exec((err,data)=>{
+       if(err){
+        return res.status(400).json({
+            error:errorHandler
+        })
+
+       }
+    })
+    res.json({data})
+}
+
+
+
+exports.deleteCategory=(req,res,next)=>{
+    const category=req.category
+    // category.name=req.body.name
+
+    category.remove((err,data)=>{
+        if(err){
+            return res.status(400).json({error:errorHandler(err)})
+        }
+        res.json({message:"category deleted"})
+    })
+
+
+}
+ 
+
+exports.updateCategory=(req,res,next)=>{
+    
+    const category=req.category
+    category.name=req.body.name
+
+    category.save((err,data)=>{
+        if(err){
+            return res.status(400).json({error:errorHandler(err)})
+        }
+        res.json(data)
+    })
+}
--- routes/category.js
+++ routes/category.js
@@ -1,12 +1,20 @@
 const express=require('express');
 const { requireSignin, isAuth, isAdmin } = require('../controller/auth');
 
-const { create } = require('../controller/category');
+const { create, categoryById, read, readList, updateCategory, deleteCategory } = require('../controller/category');
 const { userById } = require('../controller/user');
 
 
 const router=express.Router()
 
-router.post('/category/create/:userId', requireSignin ,isAuth,isAdmin,create);
+router.post('/category/create/:userId',requireSignin ,isAuth,isAdmin,create);
+router.get('/category/:categoryId',read) ;
+router.get('/categories/:categoryId',readList) ;
+
+router.delete('/category/:categoryId/:userId', requireSignin,isAuth,isAdmin,deletecategory)
+router.put('/category/:categoryId/:userId', requireSignin,isAuth,isAdmin,updatecategory)  
+
 router.param('userId',userById)
+router.param('categoryId',categoryById)
+
 module.exports=router;
